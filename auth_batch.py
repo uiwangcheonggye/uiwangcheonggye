98d51830fc43fde6b5d124d299cfb53ea4f0ce9b
uiwangcheonggye <uiwangcheonggye@gmail.com>
2025-06-29 01:06:15 +0900

    Update auth_batch.py

--- auth_batch.py
+++ auth_batch.py
@@ -75,11 +75,11 @@ def merge_into_admin_sheet():
         "타입",
         "이름",
         "비상연락망",
-        "입주계약자 확인",
-        "카페 닉네임",
         "카카오톡 안내(세대별 1인, 2인부터는 일정기간 후 참여가능)",
-        "카카오톡 참여확인(동호수 닉네임)",
-        "입주예정자협의회 위임장 제출",
+        "네이버카페 ID",
+        "카카오톡 닉네임",
+        "계약서 업로드",
+        "입주예정자협의회 위임장 업로드",
         "비고",
     ]
     admin_df = pd.DataFrame(
@@ -110,38 +110,48 @@ def merge_into_admin_sheet():
     for i, row in admin_df.iterrows():
         key = row["KEY"]
         if key in response_df.index:
-            res_list = response_df.loc[[key]]  # DataFrame 형태 유지
+            res_list = response_df.loc[[key]]
 
-            # 여러 응답이 있을 수 있으므로 loop
             for _, res in res_list.iterrows():
-                # 카카오톡 참여 확인 → 카카오톡 참여확인(동호수 닉네임)
+                if "이름" in res and "이름" in admin_df.columns:
+                    original = str(admin_df.at[i, "이름"]).strip()
+                    items = set(filter(None, [original, str(res["이름"]).strip()]))
+                    admin_df.at[i, "이름"] = ", ".join(sorted(items))
+
+                if "비상연락망" in res and "비상연락망" in admin_df.columns:
+                    original = str(admin_df.at[i, "비상연락망"]).strip()
+                    items = set(
+                        filter(None, [original, str(res["비상연락망"]).strip()])
+                    )
+                    admin_df.at[i, "비상연락망"] = ", ".join(sorted(items))
+
+                if "네이버카페 ID" in res and "네이버카페 ID" in admin_df.columns:
+                    original = str(admin_df.at[i, "네이버카페 ID"]).strip()
+                    items = set(
+                        filter(None, [original, str(res["네이버카페 ID"]).strip()])
+                    )
+                    admin_df.at[i, "네이버카페 ID"] = ", ".join(sorted(items))
+
+                if "계약서 업로드" in res and "계약서 업로드" in admin_df.columns:
+                    original = str(admin_df.at[i, "계약서 업로드"]).strip()
+                    items = set(
+                        filter(None, [original, str(res["계약서 업로드"]).strip()])
+                    )
+                    admin_df.at[i, "계약서 업로드"] = ", ".join(sorted(items))
+
                 if (
-                    "카카오톡 참여 확인" in res
-                    and "카카오톡 참여확인(동호수 닉네임)" in admin_df.columns
+                    "위임장 업로드" in res
+                    and "입주예정자협의회 위임장 업로드" in admin_df.columns
                 ):
                     original = str(
-                        admin_df.at[i, "카카오톡 참여확인(동호수 닉네임)"]
+                        admin_df.at[i, "입주예정자협의회 위임장 업로드"]
                     ).strip()
                     items = set(
-                        filter(None, [original, str(res["카카오톡 참여 확인"]).strip()])
+                        filter(None, [original, str(res["위임장 업로드"]).strip()])
                     )
-                    admin_df.at[i, "카카오톡 참여확인(동호수 닉네임)"] = ", ".join(
+                    admin_df.at[i, "입주예정자협의회 위임장 업로드"] = ", ".join(
                         sorted(items)
                     )
-
-                # 네이버카페 아이디 → 카페 안내
-                if "네이버카페 닉네임" in res and "카페 닉네임" in admin_df.columns:
-                    original = str(admin_df.at[i, "카페 닉네임"]).strip()
-                    items = set(
-                        filter(None, [original, str(res["네이버카페 닉네임"]).strip()])
-                    )
-                    admin_df.at[i, "카페 닉네임"] = ", ".join(sorted(items))
-
-                # # 위임장 업로드 → 입주예정자협의회 위임장 제출
-                # if '위임장 업로드' in res and '입주예정자협의회 위임장 제출' in admin_df.columns:
-                #     if res['위임장 업로드'].strip():
-                #         admin_df.at[i, '입주예정자협의회 위임장 제출'] = '제출'
-                #
 
     # 시트에 덮어쓰기
     # 헤더 3줄 건너뛰고, 데이터만 업데이트
